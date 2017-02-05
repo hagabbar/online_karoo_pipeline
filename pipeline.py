29b38861db29cc5940050143c2069868eef7dd1f
Hunter Gabbard <hunter.gabbard@ligo.org>
2017-02-05 09:36:24 -0800

    reading timeseries from cache function added

--- pipeline.py
+++ pipeline.py
@@ -7,12 +7,17 @@ import os
 import numpy as np
 import argparse
 from trigfind import find_trigger_files
+from gwpy.timeseries import TimeSeries
 
 
 def locate_trigs(st,et):
-    urls = trigfind.find_trigger_files('L1:GDS-CALIB_STRAIN', 'Omicron', st, et)
+    cache = trigfind.find_trigger_files('L1:GDS-CALIB_STRAIN', 'Omicron', st, et)
 
-    return urls
+    return cache
+
+def preproc(cache):
+     data = TimeSeries.read(cache, ‘L1:GDS-CALIB_STRAIN’)
+
 
 if __name__ == '__main__':
     #construct the argument parse and parse the arguments
@@ -23,7 +28,14 @@ if __name__ == '__main__':
             help="end time")
 
     #Extract trig files
-    files = locate_trigs(st,et)
+    cache = locate_trigs(st,et)
     
     #Perform pre-processing of triggers (snr > 7.5)
-    
+    trigs = preproc(cache)    
+
+    #Run script for generating features
+
+    #Process features against karoo generated multivariant expression, one for each type of glitch
+
+    #Write in an html page if gltich type of not
+ 

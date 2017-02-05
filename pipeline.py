866cb4f49342de23eef9578737064923f63258a6
Hunter Gabbard <hunter.gabbard@ligo.org>
2017-02-05 09:11:34 -0800

    locate urls function added

--- pipeline.py
+++ pipeline.py
@@ -9,9 +9,21 @@ import argparse
 from trigfind import find_trigger_files
 
 
-def locate_trigs():
-    trigfind.find_trigger_files('L1:GDS-CALIB_STRAIN', 'Omicron', 1135641617, 1135728017)
+def locate_trigs(st,et):
+    urls = trigfind.find_trigger_files('L1:GDS-CALIB_STRAIN', 'Omicron', st, et)
+
+    return urls
 
 if __name__ == '__main__':
     #construct the argument parse and parse the arguments
     ap = argparse.ArgumentParser()
+    ap.add_argument("-s", "--start-time", required=True,
+            help="start time")
+    ap.add_argument("-e", "--end-time", required=True,
+            help="end time")
+
+    #Extract trig files
+    files = locate_trigs(st,et)
+    
+    #Perform pre-processing of triggers (snr > 7.5)
+    
